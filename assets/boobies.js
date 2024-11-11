c494295e836a807abacd75128d1f3fb758d1fc5f
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-11-11 10:29:14 -0600

    spice up that menu wit a lil slider animater in + out yooo

--- assets/boobies.js
+++ assets/boobies.js
@@ -1,34 +1,17 @@
-const navLinks = [
-	{
-		name: 'home',
-		link: '/',
-	},
-	{
-		name: 'itty bitty ditties',
-		link: '/itty-bitty-ditties',
-	},
-];
-
 let isMenuOpen = false;
 const menuClosedHTML = '<p>MENU <span id="menu-tri">&#9650;</span></p>';
 const menuOpenHTML = '<p>MENU <span id="menu-tri">&#9660;</span></p>';
 const menuBtn = document.getElementById('menu');
-const menuParent = document.getElementsByTagName('nav')[0];
-const linksParent = document.createElement('ul');
-for (const navLink of navLinks) {
-	const navLinkWrap = document.createElement('li');
-	navLinkWrap.innerHTML = `<a href="${navLink.link}"><p>${navLink.name}</p></a>`;
-	linksParent.appendChild(navLinkWrap);
-};
-menuBtn.addEventListener('click', () => {
+const linksParent = document.getElementsByClassName('menu-links-init')[0];
+menuBtn.addEventListener('click', function menuButtClicker() {
 	if (!isMenuOpen) {
 		menuBtn.innerHTML = menuOpenHTML;
-		menuParent.appendChild(linksParent);
+		linksParent.className = 'menu-links-open';
 		isMenuOpen = true;
 		return;
 	}
 	menuBtn.innerHTML = menuClosedHTML;
-	linksParent.remove();
+	linksParent.className = 'menu-links-closed';
 	isMenuOpen = false;
 });
 
