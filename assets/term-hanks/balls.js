7b794b9685df857138663ac069aa7b889018ad3e
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-12-14 21:40:21 -0600

    fuck xterm we're doing it live + we got a game engine if you can call it that

---
+++ assets/term-hanks/balls.js
@@ -0,0 +1,345 @@
+const termHanks = [
+        '..........................................',
+        '..........................................',
+        '...........:=+*#@%%%#+=-:.................',
+        '.........-%@@@@@@@@@@@@@@%+:..............',
+        '.......:#@@@%@@@@@@@%%#%%%+...............',
+        '......=@@@@%@@@@@@@#=-===+-...............',
+        '......*@@@%@%@@@@@@%+-==+=+-..............',
+        ' ....+@@@@@@@@@@@@@@#=====++:.............',
+        '.....#@@@@@@@@@@@%%#+====+*#-.............',
+        '.....*@@@@@@@@*++***====++*#-.............',
+        '.....-%@@@@@@#==*=-=======**+=:...........',
+        '.....:#@@@@@#+===---======++=+=...........',
+        '......=@@%%%%#=-==========+*+=............',
+        '.......++--------=+=======+*#=............',
+        '....:+##+------==++++++++++*#-............',
+        '...:#%%%%%%---==++++*######*+:............',
+        '..-*%%%%%%%@#==++******+=-:...............',
+        '=*#%#%%%%%@%%%*=+***=:....................',
+        '%##%#%%%%%%@%%%#+*+:......................',
+        '%#%%%%%%%%%@@@@%%*-.......................',
+        '%%%%%%%%%%@@@@@@@%%*......................',
+        '%%%%%%%%%%@@@@@@@@@%:.....................',
+        '%#%%%#%%%%@@@@@@@@@@*.....................',
+        '%%%#%%%%%%@@@@@@@@@@@-....................',
+        '%%%%%%%%%@@@@@@@@@@@@%:...................',
+        '###%%%%%%%%@@@@@@@@@@@*:..................',
+        '@@@@@@@@@@@@@@@@@@@@@@@=..................',
+        '%%%%%%@@@@@@@@@@@@@@@@@%:.................',
+        '%%%%%%%%@@@@@@@@@@@@@@@@*.................',
+        '%%%%%%%%%@@@@@@@@@@@@@@@@*................',
+        '%%%%%@@@@@@@@@@@@@@@@@@@@@*...............',
+        '%%%%%@@@@@@@@@@@@@@@@@@@@@@*..............',
+        '%%@@@@@@@@@@@@@@@@@@@@@@@@@#..............',
+        '@%%%@@@@@@@@@@@@@@@@@@@@@@@*..............',
+        '@@%%%@@@@@@@@@@@@@@@@@@@@@@*..............',
+        '%%%@@@@@@@@@@@@@@@@@@@@@@@@=..............',
+        '%@@@@@%%@@@@@@@@@@@@@@@@@@%:..............',
+        '@@@@@@@@@@@@@@@@@@@@@@@@@@+...............',
+        '%%%%%%%%%%@@@@@@@@@@@@@@@%-...............',
+        '@@@@@@@@@@@@@@@@@@@@@@@@@%-....... .......',
+        '%%%%%%%%%%%%%@@%%%%%%@@@@%-...............',
+        '%%%@@@@@@@@@@@@@@%%%%%%%%%-...............',
+        '%%@%%%%%%%%%%%%@@@@%%%%@%%%+:.............',
+        '@@@@@%%%%%%%%@@@@%@@@%@@@%@@%#-...........',
+        '%@@@@%%%%@%%@@@%%%@%%%%@@@@@%%@%*:........',
+        '@@@@@@@@%%%%%@%%%%%%%%%@@@@@@%%%@%-.......',
+        '%%%%@@@@@@@%%@%%@@@@%%%@@@@@%%%%@%-.......',
+        '%%%@@@@@%%@@@@@@@@@@%%%@@@@@@@%%@%=.......',
+        '%%@@@@@%%@@@@@@@@@@%%%%@@@@@@@%@@@*.......',
+        '%%@@@@@%@@@@@@@%@@%%%@@@@@%%@@@@%@%:......',
+        '%%%@@%%%@@@@@@@%%%%%%@@@@%%%%@@@@@@-......',
+        '%%%%%%@@@@@@@@@@@%%%%@@@%@%%%@@@@@@=......',
+        '%%%%@@@@@@@@@@@@@@%%%@@@%%%%%@@@@@@#......',
+        '%%%%@@@@@@@@@@@@@@@@@@@@%%@@%%@@@@@%=.. ..',
+        '%%%@@@@@@%%@@@@@@@@@@@@@@%@%%%%@@%@@%-....',
+        '%%@@@@@@%%%@@@@@@%@@@@@@@@@@%%@@@%@@@+....',
+        '@@@@@@@%%%%@@@@@@@@@@@@@@@@@@@@@@@@@%-....',
+        '@@@@@@@%%%@@@@@@@@@@@@@%@@@@@@@@@@@#:.....',
+        '@@@@@@%%%%@@@@@@@@@@@@@@@@@@@@@@@#=.......',
+        '@@@%%%%%%%@@@@@@@@@@@@@@@@%%%#*=:.........',
+        '@%%%%@%%%%@@@@@@@#-:.::...................',
+        '@%%%@@%%%%@@@@@@@@@@%%@%%##%%%*=:.........',
+        '%%%@@%%%%%@@@@@@@@@@@@@@@@@@@@@@@+........',
+        '%%@@%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@+.......',
+        '@@@%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@*.......',
+        '@@@%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@%-.......',
+        '@@@%%%%%%%%%@@@@@@@@@@@@@@@@@@@@%%:.......',
+        '@@%%%%%%%%%%@@@@@@@@@@@@@@@@@@@%@#:.......',
+        '@@%%%%%%%%%@@@@@@@@@@@@@@@@@@@%@@#:.......',
+        '@%%%%%%%%%%@@@@@@@@@@@@@@@@@@%%@@*........',
+        '@%%%%%%%%%%%@@@@@@@@@@@@@@@@%%%@@*........',
+        '%%%%%%%%%%%@@%@@@@@@@@@@@@@@%%@@@+........',
+        '%%%%%%%%%%%%%%@@@@@@@@@@@@@@%%@@@+........',
+        '%%%%%@%%%%%%%%@@@@@@@@@@%@@@%@@@@+........',
+        '@@@@@@@@@@@@@@@@@@@@@@@%%@@@@@@@@+........',
+        '@@@@@@@@@@@@@@@@@@@@@@@%@@@@%@%@%=........',
+        '%%@@@@@@@@@@@@@@@@@@@@@@@@@@%@@@%-........',
+        '@@@@@@@@@@@@@@@@@@@@%@@%%@@@%@@@%-........',
+        '@@@@@@@@@@@@@@@@@@@@@@@@%@@@%@@@%:........',
+        '@@@@@@@@@@@@@@@@@@@@@%@@@@@@%@@@#:........',
+        '@@@@@@@@@@@@@@@@%%%@@%@@@@@@@@@@*:........',
+        '@@@@@@@@@@@@@@@@%%@@@@%@@@@@@@%@*.........',
+        '@@@@@@@@@@@@@@@@%@@@@@@@@@@@%@@@+.........',
+        '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#:.........',
+        '@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@%:.........',
+        '@@@@@@@@@@@@@@@@@%@@@@@@%%%@%%%*..........',
+        '@@@%%@@@@@@@@@@@@@@@@@@%%%%@%@%+..........',
+        '@@@%%@@@@@@@@%%%@@@@@@@@@@@@@@@=..........',
+        '@@%%%%@@@@@@*...........:...::............',
+        '%%%%%%@@@@@%-.... ........................',
+        '@%%@%%%@@@@*..............................',
+        '%%%@@%%@@@@+..............................',
+        '@@@@@%%@@@@#..............................',
+        '@@@@@@@%@@@%:.............................',
+        '@@@@@@@@@@@%:.............................',
+        '@%%@@@@%@@@%:.............................',
+        '@@@@@@@@@@@%=.............................',
+        '@@@@@@@@@@@@@%-.:.::::::::::::::::::::::::',
+        '@@@@@@@@@@@@@@%=::::::::::::::::::::::::::',
+        '%@@@@@@@@@@@@@@%=:::::::::::::::::::::::::',
+        '@@@@@@@@@@@@@@@@%=:-:-::::::::::::::::::::',
+        '@@@@@@@@@@@@%@@@@%=::::::::::::::::::::...',
+        '@%%%%%@@@@%@@%@@@@%%#*=-:::.:.............',
+        '%%@@@%@@%%##%%##%%%%%%%%%%%#=.............',
+        '%%%%%%%%%%%@%%%%%%%%%%%%%%@%%-............',
+        '##%%%%%%%%##%%%%%%%%%%%%%##+-.............',
+        '..........................................',
+        '..........................................',
+        '..........................................',
+]
+
+const farts = {
+        something: [
+                '..........................................',
+                '++=:......................................',
+                '++====....................................',
+                '++++=++:.............. ..................+',
+                '++++++++:.........&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.............+',
+                '*++++=++=:.......&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...........*',
+                '***+++=++==....&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...............*',
+                '**++++==+++=-..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..::::::---------*',
+                '+*++++=======--........::::::::------====*',
+                '+****+++++++++=====-::::::::::-----======*',
+                '*****++++*+++++===========----===========*',
+                '*******++**+=+++=+====+++++++++++-===++++*',
+                '**#******++++++++=+**++===++++****+++++++*',
+                '##***#*****+**+++*=::::::++++++++++****#**',
+                '##*##***##****#*+-:::::..-*##*=+++++**#**#',
+                '*###%#**#***+**+*+::::.:..::-=**+=+=+++++#',
+                '*#*###**#**+**###+:---::::::::+##*##+*##*+',
+                '##*#######*##*#%%=----:-----::--*#*******#',
+                '#***#*#%##%%%%%*=----------------=+++*****',
+                '##%%%%%%%%%%#+==-----------------+*#*+**+#',
+                '%@@@%%%#*+++============---------+#******+',
+                '%%%#*+++=====================-===##***#***',
+                '**++=============================*##%#***#',
+                '+++==========================++**####**##+',
+                '+++++++++++++++++++++++++++*#**%#%###***##',
+                '*****+++++++++++++++++++++**+*#***##**###*',
+                '**********+++++++++++++++*+**#*#####%%%%##',
+                '************#%%%#**+*******#***#####**%%##',
+                '*#**********#%%%%**+***############*******',
+                '#####*#######*%%##%*%#######%%#%##%##*#***',
+        ],
+        'something else': [],
+        'something else entirely': [],
+}
+
+const thingers = {
+        tester: {
+                art: 'something',
+                text: 'this is the text yo what about it',
+                choices: [
+                        {
+                                text: 'sup yo',
+                                next: 'tester lester',
+                        },
+                        {
+                                text: 'oh no yo whoa bro',
+                                next: 'tester mester',
+                        },
+                ],
+        },
+        'tester lester': {
+                art: 'something else',
+                text: 'this is tester lester the bester let it fester',
+                choices: [
+                        {
+                                text: 'ok bro what gives',
+                                next: 'tester mester',
+                        },
+                        {
+                                text: 'live and let live my man',
+                                next: 'tester',
+                        },
+                ],
+        },
+        'tester mester': {
+                art: 'something else entirely',
+                text: 'and this is tester mester what could be better',
+                choices: [
+                        {
+                                text: 'yo yoy oy oy oya',
+                                next: 'tester lester',
+                        },
+                        {
+                                text: 'live and let live my man',
+                                next: 'tester',
+                        },
+                ],
+        },
+}
+
+const termHanksLine = document.getElementsByClassName("term-hanks-line")
+
+const NUMB_COLS = termHanks[0].length
+const NUMB_ROWS = termHanksLine.length
+
+function renderTermasHankyPanky(startRow, endRow, timeout, res) {
+        if (startRow >= 0) {
+                if (timeout > 50) {
+                        timeout -= 5
+                }
+                for (let i = startRow, j = 0; i < endRow; i++, j++) {
+                        const htmlLine = termHanksLine[j]
+                        const lineToRender = termHanks[i]
+                        htmlLine.textContent = lineToRender
+                }
+                return setTimeout(() => {
+                        renderTermasHankyPanky(startRow - 1, endRow - 1, timeout, res)
+                }, timeout)
+        }
+        return res()
+}
+async function animateTermiasHankas() {
+        const lastRow = termHanks.length - 1
+        const rowsToRender = NUMB_ROWS
+        const startTimeout = 200
+        const startRow = lastRow - rowsToRender
+        await new Promise(res => {
+                renderTermasHankyPanky(startRow, lastRow, startTimeout, res)
+        })
+}
+requestAnimationFrame(animateTermiasHankas)
+
+const cursorHtml = '<span>&#9646;</span>'
+const cursorBlinkHtml = '<span class="cursor">&#9646;</span>'
+
+const introText = 'Hello, I\'m Term Hanks. I\'m stuck in this terminal due to geopolitical events beyond my control'
+
+const startText = 'Will you help me get home?'
+
+function formatText(text) {
+        const words = text.split(" ")
+        const lines = []
+        for (let i = 0, line = ""; i < words.length; i++) {
+                const word = words[i]
+                const spaceWord = " " + word
+                if ((spaceWord + line).length > NUMB_COLS - 1) {
+                        lines.push(line)
+                        line = word
+                } else {
+                        line += spaceWord
+                }
+                if (i === words.length - 1) {
+                        lines.push(line)
+                }
+        }
+        return lines
+}
+
+async function typeTextLine(text, el, isEndOfTextBlock) {
+        const timeout = 80
+        for (let i = 1; i < text.length + 1; i++) {
+                await new Promise(res => setTimeout(() => res(), timeout))
+                const letters = text.slice(0, i);
+                el.innerHTML = letters + cursorHtml;
+        }
+        if (!isEndOfTextBlock) {
+                await new Promise(res => setTimeout(() => res(), timeout))
+                el.innerHTML = text
+                return
+        }
+        el.innerHTML = text + cursorBlinkHtml
+}
+
+async function typeText(text, startLineIndex) {
+        const lines = formatText(text)
+        const endIndie = lines.length - 1
+        for (let i = 0, x = startLineIndex; i < lines.length; i++, x++) {
+                const line = lines[i]
+                const el = termHanksLine[x]
+                await typeTextLine(line, el, i === endIndie)
+        }
+        return startTextIndie + endIndie
+}
+
+function renderChoice(choice, indie) {
+        const button = document.createElement('button')
+        button.innerText = choice.text
+        button.className = 'term-butt'
+        button.onclick = () => {
+                const nextPanel = thingers[choice.next]
+                renderTextPanel(nextPanel)
+        }
+        const el = termHanksLine[indie]
+        el.replaceChildren(button)
+}
+
+function clearTerm() {
+        let breaker = ''
+        for (let i = 0; i < NUMB_COLS; i++) {
+                breaker += '&nbsp;'
+        }
+        for (let i = 0; i < termHanksLine.length; i++) {
+                const line = termHanksLine[i]
+                line.innerHTML = breaker
+        }
+}
+
+const startTextPanelIndie = termHanksLine.length - 12
+const startTextIndie = startTextPanelIndie + 2
+console.log('start the text panel', startTextPanelIndie)
+
+function clearTextPanel() {
+        for (let i = startTextPanelIndie; i < termHanksLine.length; i++) {
+                const htmlLine = termHanksLine[i]
+                if (i === startTextPanelIndie) {
+                        htmlLine.innerText = '------------------------------------------'
+                } else {
+                        htmlLine.innerHTML = '&nbsp;'
+                }
+        }
+}
+
+async function renderTextPanel(panel) {
+        const art = farts[panel.art]
+        renderFart(art)
+        clearTextPanel()
+        const ender = await typeText(panel.text, startTextIndie)
+        await new Promise(res => setTimeout(() => res(), 200))
+        for (let i = 0; i < panel.choices.length; i++) {
+                const choice = panel.choices[i]
+                renderChoice(choice, ender + ((i + 1) * 2))
+        }
+}
+
+function renderFart(fart) {
+        for (let i = 0; i < startTextPanelIndie; i++) {
+                const htmlLine = termHanksLine[i]
+                const fartLine = fart[i]
+                if (fartLine) {
+                        htmlLine.innerHTML = fartLine
+                }
+        }
+}
+
+animateTermiasHankas().then(async () => {
+        clearTextPanel()
+        await typeText(introText, startTextIndie)
+        await new Promise(res => setTimeout(() => res(), 3000))
+        const startPanel = thingers.tester
+        renderTextPanel(startPanel)
+})
